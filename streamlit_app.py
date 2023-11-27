0cb146a37111cb632ef3688c7795624177c086c4
Chromoshadow <79143422+Chromoshadow@users.noreply.github.com>
2023-11-27 16:03:48 +0100

    correction : moved emoji within the strings

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My parent healthy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text(🥣 'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗 'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐔 'Hard-Boiled Free-Range Egg')
-streamlit.text(🥑🍞 'Avocado toast')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
