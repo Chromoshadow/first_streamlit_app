2473d161e4fd38bcd2c611dc36daec3fcf47ed83
Chromoshadow <79143422+Chromoshadow@users.noreply.github.com>
2023-11-30 13:48:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,4 +60,3 @@ if streamlit.button('Add a fruit to the list'):
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
 
-def insert-row_snowflake(new_fruit):
